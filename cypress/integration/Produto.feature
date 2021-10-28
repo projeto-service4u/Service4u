37ae768710fcebeee979d428057b57af5571366f
Matheus Angellos <40271395+matheusangellos@users.noreply.github.com>
2021-10-28 10:33:56 -0300

    ✅ #S4U-27 | Adicionando automações de Produto

--- cypress/integration/Produto.feature
+++ cypress/integration/Produto.feature
@@ -11,3 +11,30 @@ Scenario: Validacoes gerais da tela de Login
     And o botão Adicionar Item
     And visualizar as colunas "Nome" e "Unidade/Medida"
     And a listagem de itens já cadastrados ou não
+
+Scenario: Validar itens do modal
+    Given que sou um prestador cadastrado
+    When acesso a tela de Produtos
+    And clico no botão Adicionar item
+    Then devo visualizar o modal com o título "Adicionar Item"
+    And os campos Nome "Produto" e "Unidade/Medida"
+    And os botões de Enviar e Fechar
+
+Scenario: Validar fechamento do modal
+    Given que sou um prestador cadastrado
+    When acesso a tela de Produtos
+    And clico no botão Adicionar item
+    Then ao clicar no botão Fechar devo voltar para a tela de "Produtos"
+
+Scenario: Validar botão Enviar desabilitado
+    Given que sou um prestador cadastrado
+    When acesso a tela de Produtos
+    And clico no botão Adicionar item
+    Then ao deixar os campos vazios o botão Enviar estar desabilitado
+
+Scenario: Cadastro de produto com sucesso
+    Given que sou um prestador cadastrado
+    When acesso a tela de Produtos
+    And clico no botão Adicionar item
+    Then após preencher os dados de "Nome" e "Unidade/Medida" e clicar em Enviar devo receber uma mensagem de sucesso
+    And visualizar na listagem após fechar o modal
--- cypress/integration/Produto/steps.js
+++ cypress/integration/Produto/steps.js
@@ -4,8 +4,6 @@ Given(/^que sou um prestador cadastrado$/, () => {
 
 When(/^efetuo login com sucesso$/, () => {
   cy.visit('/')
-  cy.get('#email').type('admin@admin.com')
-  cy.get('#senha').type('123456{enter}')
 })
 
 When(/^clico no menu Produto$/, () => {
@@ -27,4 +25,57 @@ And(/^visualizar as colunas "([^"]*)" e "([^"]*)"$/, (Nome, args2) => {
 
 And(/^a listagem de itens já cadastrados ou não$/, () => {
   cy.get('.sc-fubCzh > .MuiPaper-root').should('be.visible')
+})
+
+When(/^acesso a tela de Produtos$/, () => {
+  cy.visit('/')
+  cy.get('[href="/produtos"]').click()
+})
+
+When(/^clico no botão Adicionar item$/, () => {
+  cy.get('.MuiButtonBase-root').click()
+})
+
+Then(/^devo visualizar o modal com o título "([^"]*)"$/, () => {
+  cy.get('#contained-modal-title-vcenter').should('contain', 'Adicionar Item')
+})
+
+And(/^os campos Nome "([^"]*)" e "([^"]*)"$/, () => {
+  cy.get('.row > :nth-child(1)').should('be.visible').contains('Nome Produto')
+  cy.get('.row > :nth-child(2)').should('be.visible').contains('Unidade/Medida')
+})
+
+And(/^os botões de Enviar e Fechar$/, () => {
+  cy.get('.col-lg-2').should('be.visible').contains('Enviar')
+  cy.get('.modal-footer > .btn').should('be.visible').contains('Fechar')
+})
+
+Then(/^ao clicar no botão Fechar devo voltar para a tela de "([^"]*)"$/, () => {
+  cy.get('.modal-footer > .btn').click()
+  cy.get('.modal').should('not.exist')
+})
+
+Then(/^ao deixar os campos vazios o botão Enviar estar desabilitado$/, () => {
+  cy.get(':nth-child(1) > .form-control').should('be.empty')
+  cy.get(':nth-child(2) > .form-control').should('be.empty')
+  cy.get('.btn.btn-primary.btn-lg').should('be.disabled')
+})
+
+Then(
+  /^após preencher os dados de "([^"]*)" e "([^"]*)" e clicar em Enviar devo receber uma mensagem de sucesso$/,
+  () => {
+    cy.get(':nth-child(1) > .form-control').type('Tinta Fosca Preta')
+    cy.get(':nth-child(2) > .form-control').type('10 Litros')
+    cy.get('.btn.btn-primary.btn-lg').click()
+    cy.get('.Toastify__toast-body').should(
+      'contain',
+      'Produto adicionado com sucesso'
+    )
+  }
+)
+
+And(/^visualizar na listagem após fechar o modal$/, () => {
+  cy.get('.modal-footer > .btn').click()
+  cy.get('.sc-fubCzh > .MuiPaper-root').should('contain', 'Tinta Fosca Preta')
+  cy.get('.sc-fubCzh > .MuiPaper-root').should('contain', '10 Litros')
 })
